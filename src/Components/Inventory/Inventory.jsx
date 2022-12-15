ed9adabd87a73e899981d8fe0f66a191e3807e35
Misha_Lipovka <miwist@mail.ru>
2022-12-16 02:15:33 +0500

    new functional

--- src/Components/Inventory/Inventory.jsx
+++ src/Components/Inventory/Inventory.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import cl from "./Inventory.module.scss"
-import Table from './Table'
+import React from "react";
+import cl from "./Inventory.module.scss";
+import Table from "./Table";
+import Img from "../../img/Img.png";
 
 const Inventory = () => {
   return (
     <div className={cl.container}>
-        <div className={cl.Inventory}>
-            <div className={cl.Inventory_menu}></div>
-            <Table />
+      <div className={cl.Inventory}>
+        <div className={cl.Inventory_menu}>
+          <img src={Img} alt="img" />
+          <div className={cl.Inventory_menu__text}>text</div>
         </div>
-        
+        <Table />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
